3bd77e7f4fd41dfaf6dba6aeec020b6f57c2896c
Amgad Ahmed Sawd Alakhadher <35864592+AmgadAlakhadher@users.noreply.github.com>
2023-12-21 06:18:07 +0300

    Update Requests.sql
    .

--- Requests.sql
+++ Requests.sql
@@ -1,12 +1,10 @@
 --1. Вывести менеджеров у которых имеется номер телефона
 
-SQL
 SELECT * FROM Managers WHERE Phone IS NOT NULL;
 
 
 --2. Вывести кол-во продаж за 20 июня 2021
 
-SQL
 SELECT COUNT(*) AS SalesCount
 FROM Sells
 WHERE Date = '2021-06-20';
@@ -14,7 +12,6 @@ WHERE Date = '2021-06-20';
 
 --3. Вывести среднюю сумму продажи с товаром 'Фанера'
 
-SQL
 SELECT AVG(Sum) AS AverageSaleAmount
 FROM Sells
 WHERE ID_Prod = (SELECT ID FROM Products WHERE Name = 'Фанера');
@@ -22,7 +19,6 @@ WHERE ID_Prod = (SELECT ID FROM Products WHERE Name = 'Фанера');
 
 --4. Вывести фамилии менеджеров и общую сумму продаж для каждого с товаром 'ОСБ'
 
-SQL
 SELECT m.Fio AS ManagerName, SUM(s.Sum) AS TotalSalesAmount
 FROM Managers m
 JOIN Sells s ON m.ID = s.ID_Manag
@@ -32,7 +28,6 @@ GROUP BY m.Fio;
 
 --5. Вывести менеджера и товар, который продали 22 августа 2021
 
-SQL
 SELECT m.Fio AS ManagerName, p.Name AS SoldProduct 
 FROM Managers m
 JOIN Sells s ON m.ID = s.ID_Manag
@@ -42,13 +37,11 @@ WHERE s.Date = '2021-08-22';
 
 --6. Вывести все товары, у которых в названии имеется 'Фанера' и цена не ниже 1750
 
-SQL
 SELECT * FROM Products WHERE Name LIKE '%Фанера%' AND Cost >= 1750;
 
 
 --7. Вывести историю продаж товаров, группируя по месяцу продажи и наименованию товара
 
-SQL
 SELECT MONTH(Date) AS SaleMonth, Name AS ProductName, SUM(Count) AS TotalCount
 FROM Sells
 JOIN Products ON Sells.ID_Prod = Products.ID
@@ -57,7 +50,6 @@ GROUP BY MONTH(Date), Name;
 
 --8. Вывести количество повторяющихся значений и сами значения из таблицы 'Товары', где количество повторений больше 1.
 
-SQL
 SELECT Name, COUNT(*) AS Repetitions
 FROM Products
 GROUP BY Name
